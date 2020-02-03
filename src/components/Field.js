2cbe20923078adc6adcaf788fddcb7e6bb35cdfc
Nenad Burmazović <burmazovic@gmail.com>
2020-02-03 10:33:32 +0100

    Renaming TableFields into Fields. Moving Table and Table.css into components/Table directory. Modified CSS.

---
+++ src/components/Field.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+class TableFields extends React.Component {
+    getStyle = (piece) => {
+        return {
+            color: piece === piece.toLowerCase()? 'black':'white'
+        }
+    }
+
+    render() {
+        const draw = this.props.fields.reverse().map((chessField) => (
+            <div className="flexChild" style={this.getStyle(chessField.piece)} key={chessField.id}>{this.props.piece[chessField.piece]}</div>));
+
+        return (
+            <div className="flexContainer">
+                {draw}
+            </div>
+        );
+    }
+}
+
+export default TableFields;
