ba461cebe8811ba0a3a90cde24cb0137e3cf4a33
Nenad Burmazović <burmazovic@gmail.com>
2020-02-04 19:47:03 +0100

    CHESS-6 Task. Added .map to a function so it could return an object.

--- src/components/Field.js
+++ src/components/Field.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {fenToArray} from '../helpers/FenToArray'
 
 // FEN primer
 const fen = '3K4/8/8/8/8/8/8/3k4';
@@ -9,26 +10,12 @@ class Field extends React.Component {
             color: piece === piece.toLowerCase()? 'black':'white'
         }
     };
-    // FEN to Array
-    fenToArray = fenValue => {
-        let fenArray = [];
-        fenValue.split('').forEach( element => {
-            if (Number.isInteger(parseInt(element))) {
-                for (let i = 0; i < parseInt(element); i++) {
-                    fenArray.push('')
-                }
-            } else {
-                fenArray.push(element)
-            }
-        });
-        return fenArray.filter(piece => piece !== '/')
-    };
 
     render() {
         const draw = this.props.fields.reverse().map((chessField) => (
             <div className="flexChild" style={this.getStyle(chessField.piece)} key={chessField.id}>{this.props.piece[chessField.piece]}</div>));
         // FEN to Array TEST
-        console.log(this.fenToArray(fen));
+        console.log(fenToArray(fen));
         return (
             <div className="flexContainer">
                 {draw}
