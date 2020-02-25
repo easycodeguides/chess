3394ce217d5182d577e11c839e9e3a3f708b2547
Zeljkos <zeljkos@ever-near.com>
2020-02-25 17:12:10 +0100

    move with eating logic

--- src/components/Field.js
+++ src/components/Field.js
@@ -27,11 +27,21 @@ class Field extends React.Component {
         console.log(move1);
         const move2 = move('e6','rnbqkbnr/pppppppp/8/8/4P3/8/PPPP1PPP/RNBQKBNR');
         console.log(move2);
-        const move3 = move('Be3','rnbqkbnr/pppp1ppp/4p3/8/4P3/8/PPPP1PPP/RNBQKBNR');
+        const move3 = move('Nc3','rnbqkbnr/pppp1ppp/4p3/8/4P3/8/PPPP1PPP/RNBQKBNR');
         console.log(move3);
-        const move4 = move('bd6','rnbqkbnr/pppp1ppp/4p3/8/4P3/4B3/PPPP1PPP/RN1QKBNR');
+        const move4 = move('bd6','rnbqkbnr/pppp1ppp/4p3/8/4P3/2N5/PPPP1PPP/R1BQKBNR');
         console.log(move4);
-      
+        const move5 = move('Nd5','rnbqk1nr/pppp1ppp/3bp3/8/4P3/2N5/PPPP1PPP/R1BQKBNR');
+        console.log(move5);
+        const move6 = move('exd5','rnbqk1nr/pppp1ppp/3bp3/3N4/4P3/8/PPPP1PPP/R1BQKBNR');
+        console.log(move6);
+        const move7 = move('h3','rnbqk1nr/pppp1ppp/3b4/3p4/4P3/8/PPPP1PPP/R1BQKBNR');
+        console.log(move7);
+        const move8 = move('d3','rnbqk1nr/pppp1ppp/3b4/3p4/4P3/7P/PPPP1PP1/R1BQKBNR');
+        console.log(move8);
+        const move9 = move('Bxd3','rnbqk1nr/ppp2ppp/3b4/3p4/4P3/3p3P/PPPP1PP1/R1BQKBNR');
+        console.log(move9);
+
         return (
             <div className="flexContainer">
                 {draw}
--- src/helpers/Move.js
+++ src/helpers/Move.js
@@ -178,11 +178,156 @@ export const move = (move, position) => {
                 newBishopPos[0].piece = 'b';
             }
         }
-    }
+    } else if(move.length === 4){
+        
+        let piece = move[0], moveColumn = move[2], moveRow = move[3];
+        let columnInd;
+        switch(moveColumn){
+            case 'a':
+                columnInd = 0;
+                break;
+            case 'b':
+                columnInd = 1;
+                break;
+            case 'c':
+                columnInd = 2;
+                break;
+            case 'd':
+                columnInd = 3;
+                break;
+            case 'e':
+                columnInd = 4;
+                break;
+            case 'f':
+                columnInd = 5;
+                break;
+            case 'g':
+                columnInd = 6;
+                break;
+            case 'h':
+                columnInd = 7;
+                break;
+            default:
+                columnInd = 0;
+                break;   
+        }
+
+        if(players[currentPlayer] === 'white'){
+            
+            if(piece === 'Q') {              
+                let allQueens = fields.filter(field => field.piece === 'Q');
+                allQueens[0].piece = '';
+                let newColumnFields = fields.filter(field => field.column === moveColumn);
+                let newPosition = newColumnFields.filter(field => field.row === Number(moveRow));
+                newPosition[0].piece = 'Q';
+            } else if(piece === 'K') {
+                let allKings = fields.filter(field => field.piece === 'K');
+                allKings[0].piece = '';
+                let newColumnFields = fields.filter(field => field.column === moveColumn);
+                let newPosition = newColumnFields.filter(field => field.row === Number(moveRow));
+                newPosition[0].piece = 'K';
+            } else if(piece === 'R'){
+                let allRooks = fields.filter(field => field.piece === 'R');
+                let goodRook = allRooks.filter(rook => (rook.column === moveColumn || rook.row === moveRow));
+                goodRook[0].piece = '';
+                let newColumnFields = fields.filter(field => field.column === moveColumn);
+                let newPosition = newColumnFields.filter(field => field.row === Number(moveRow));
+                newPosition[0].piece = 'R';
+            } else if(piece === 'N'){ 
+                let eightPositions = fields.filter(field => ((field.columnIndex === (columnInd - 2)) && (field.row === (Number(moveRow) + 1))) ||
+                                                            ((field.columnIndex === (columnInd - 2)) && (field.row === (Number(moveRow) - 1))) ||
+                                                            ((field.columnIndex === (columnInd - 1)) && (field.row === (Number(moveRow) + 2))) ||
+                                                            ((field.columnIndex === (columnInd - 1)) && (field.row === (Number(moveRow) - 2))) ||
+                                                            ((field.columnIndex === (columnInd + 1)) && (field.row === (Number(moveRow) + 2))) ||
+                                                            ((field.columnIndex === (columnInd + 1)) && (field.row === (Number(moveRow) - 2))) ||
+                                                            ((field.columnIndex === (columnInd + 2)) && (field.row === (Number(moveRow) + 1))) ||
+                                                            ((field.columnIndex === (columnInd + 2)) && (field.row === (Number(moveRow) - 1))) );
+                let randomKnight = eightPositions.filter(field => field.piece === 'N');
+                randomKnight[0].piece = '';
+
+                let newColumnFields = fields.filter(field => field.column === moveColumn);
+                let newPosition = newColumnFields.filter(field => field.row === Number(moveRow));
+                newPosition[0].piece = 'N';
+            } else if(piece === 'B'){
+               
+                let fourBlocks = fields.filter(field => ((field.columnIndex !== columnInd && field.row !== Number(moveRow))
+                                                            && (field.columnIndex < columnInd && field.row > Number(moveRow) && (field.columnIndex - columnInd) === (Number(moveRow) - field.row)))
+                                                        || ((field.columnIndex !== columnInd && field.row !== Number(moveRow))
+                                                            && (field.columnIndex > columnInd && field.row > Number(moveRow) && (field.columnIndex - columnInd) === (field.row - Number(moveRow))))
+                                                        || ((field.columnIndex !== columnInd && field.row !== Number(moveRow))
+                                                            && (field.columnIndex < columnInd && field.row < Number(moveRow) && (columnInd - field.columnIndex) === (Number(moveRow) - field.row)))
+                                                        || ((field.columnIndex !== columnInd && field.row !== Number(moveRow))
+                                                            && (field.columnIndex > columnInd && field.row < Number(moveRow) && (field.columnIndex - columnInd) === (Number(moveRow) - field.row))));
+                let startBishopPosition = fourBlocks.filter(field => field.piece === 'B');
+                startBishopPosition[0].piece = '';
+                let newBishopPos = fields.filter(field => field.columnIndex === columnInd && field.row === Number(moveRow));
+                newBishopPos[0].piece = 'B';
+            } else if(piece === 'e'){
+                var startPawnPosition = fields.filter(field => ((field.column === piece) && (field.row === (Number(moveRow) - 1))));
+                startPawnPosition[0].piece = '';
+                var eatPosition = fields.filter(field => field.column === moveColumn && field.row === Number(moveRow));
+                eatPosition[0].piece = 'P';
+            }
+        } else {
+            if(piece === 'q') {              
+                let allQueens = fields.filter(field => field.piece === 'q');
+                allQueens[0].piece = '';
+                let newColumnFields = fields.filter(field => field.column === moveColumn);
+                let newPosition = newColumnFields.filter(field => field.row === Number(moveRow));
+                newPosition[0].piece = 'q';
+            } else if(piece === 'k') {
+                let allKings = fields.filter(field => field.piece === 'k');
+                allKings[0].piece = '';
+                let newColumnFields = fields.filter(field => field.column === moveColumn);
+                let newPosition = newColumnFields.filter(field => field.row === Number(moveRow));
+                newPosition[0].piece = 'k';
+            } else if(piece === 'r'){
+                let allRooks = fields.filter(field => field.piece === 'r');
+                let goodRook = allRooks.filter(rook => (rook.column === moveColumn || rook.row === moveRow));
+                goodRook[0].piece = '';
+                let newColumnFields = fields.filter(field => field.column === moveColumn);
+                let newPosition = newColumnFields.filter(field => field.row === Number(moveRow));
+                newPosition[0].piece = 'r';
+            } else if(piece === 'n'){ 
+                let eightPositions = fields.filter(field => ((field.columnIndex === (columnInd - 2)) && (field.row === (Number(moveRow) + 1))) ||
+                                                            ((field.columnIndex === (columnInd - 2)) && (field.row === (Number(moveRow) - 1))) ||
+                                                            ((field.columnIndex === (columnInd - 1)) && (field.row === (Number(moveRow) + 2))) ||
+                                                            ((field.columnIndex === (columnInd - 1)) && (field.row === (Number(moveRow) - 2))) ||
+                                                            ((field.columnIndex === (columnInd + 1)) && (field.row === (Number(moveRow) + 2))) ||
+                                                            ((field.columnIndex === (columnInd + 1)) && (field.row === (Number(moveRow) - 2))) ||
+                                                            ((field.columnIndex === (columnInd + 2)) && (field.row === (Number(moveRow) + 1))) ||
+                                                            ((field.columnIndex === (columnInd + 2)) && (field.row === (Number(moveRow) - 1))) );
+                let randomKnight = eightPositions.filter(field => field.piece === 'n');
+                randomKnight[0].piece = '';
+
+                let newColumnFields = fields.filter(field => field.column === moveColumn);
+                let newPosition = newColumnFields.filter(field => field.row === Number(moveRow));
+                newPosition[0].piece = 'n';
+            } else if(piece === 'b'){
+                let fourBlocks = fields.filter(field => ((field.columnIndex !== columnInd && field.row !== Number(moveRow))
+                                                            && (field.columnIndex > columnInd && field.row < Number(moveRow) && (field.columnIndex - columnInd) === (Number(moveRow) - field.row)))
+                                                        || ((field.columnIndex !== columnInd && field.row !== Number(moveRow))
+                                                            && (field.columnIndex < columnInd && field.row < Number(moveRow) && (columnInd - field.columnIndex) === (Number(moveRow) - field.row)))
+                                                        || ((field.columnIndex !== columnInd && field.row !== Number(moveRow))
+                                                            && (field.columnIndex > columnInd && field.row > Number(moveRow) && (field.columnIndex - columnInd) === (field.row - Number(moveRow))))
+                                                        || ((field.columnIndex !== columnInd && field.row !== Number(moveRow))
+                                                            && (field.columnIndex < columnInd && field.row > Number(moveRow) && (columnInd - field.columnIndex) === (field.row - Number(moveRow)))));
+                let startBishopPosition = fourBlocks.filter(field => field.piece === 'b');
+                startBishopPosition[0].piece = '';
+                let newBishopPos = fields.filter(field => field.columnIndex === columnInd && field.row === Number(moveRow));
+                newBishopPos[0].piece = 'b';
+            } else if(piece === 'e'){
+                var startPawnPositionB = fields.filter(field => ((field.column === piece) && (field.row === (Number(moveRow) + 1))));
+                startPawnPositionB[0].piece = '';
+                var eatPositionB = fields.filter(field => field.column === moveColumn && field.row === Number(moveRow));
+                eatPositionB[0].piece = 'p';
+            }
+        }
+    } 
     changePlayer();
     
     let fen = arrayToFen(fields);
-    return fen;   
+    return fen;
 }
 
 function changePlayer() {
